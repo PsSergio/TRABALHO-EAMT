3796e824130a13a61a8a02660d7c476909b14f4d
Paulo Sergio <pscosta2008@gmail.com>
2023-10-01 13:38:15 -0300

    feat: Adicionando funcionalidades nos botões com abas

--- SITE/js/script.js
+++ SITE/js/script.js
@@ -1,4 +1,13 @@
 // HEADER
+
+function TiraDropDown(elemento){
+
+    for(let i = 0; i < elemento.length; i++){
+        elemento[i].style.display = 'none'
+    }
+
+}
+
 const topicoComAbas = document.getElementsByClassName("topico-com-abas");
 const dropDown = document.getElementsByClassName("dropDown");
 let abaOpen = false;
@@ -7,13 +16,12 @@ for(let i = 0; i < topicoComAbas.length; i++){
 
     topicoComAbas[i].addEventListener("mouseover", () =>{ // muda direcao da setinha
         document.getElementsByClassName("arrow-navBar")[i].style.rotate = '90deg';
-        dropDown[0].style.display = 'none';
-        dropDown[1].style.display = 'none';
+        TiraDropDown(dropDown)
         abaOpen = false;
     })
 
-    document.getElementsByClassName("topico-com-abas-container")[i].addEventListener("mouseout", () =>{ // direcao original da setinha
-        document.getElementsByClassName("arrow-navBar")[i].style.rotate = '0deg';
+    document.getElementsByClassName("topico-com-abas")[i].addEventListener("mouseout", () =>{ // direcao original da setinha
+    document.getElementsByClassName("arrow-navBar")[i].style.rotate = '0deg';
     })
 
     topicoComAbas[i].addEventListener("click", () =>{
@@ -23,8 +31,7 @@ for(let i = 0; i < topicoComAbas.length; i++){
             return
         }
 
-        dropDown[0].style.display = 'none';
-        dropDown[1].style.display = 'none';
+        TiraDropDown(dropDown);
 
         abaOpen = false;
         
@@ -49,5 +56,39 @@ for(let i = 0; i < inputs.length; i++){ // funcao para pegar valor de input e de
         }
         inputValue = inputValue.toFixed(2) // funcao nativa para arredondar a 2 casas decimais (o valor precisa estar em numeros)
         console.log(inputValue)
+    })
+}
+
+// dropDown opcoes da tabela
+
+const opButtonCalculo = document.getElementsByClassName("button-op-calculation-container");
+const dropDownCalculo = document.getElementsByClassName("dropDown-calculo");
+
+let abaButtonOpen = false 
+
+for(let i = 0; i < opButtonCalculo.length; i++){
+
+    opButtonCalculo[i].addEventListener("click", () =>{
+        if(!abaButtonOpen){
+            dropDownCalculo[i].style.display = 'block'
+            abaButtonOpen = true;
+            return
+        }
+
+        TiraDropDown(dropDownCalculo)
+        abaButtonOpen = false;
+
+    })
+
+}
+
+const optionsDropDownCalculo = ['PRICE', 'SAC', 'SACRE', 'MEJS'];
+const ElementLiDropDownCalculo1 = document.getElementsByClassName("op-dropDown-calculo1");
+// terminar
+
+for(let i = 0; i < ElementLiDropDownCalculo1.length; i++){
+    ElementLiDropDownCalculo1[i].addEventListener("click", () =>{
+        document.getElementById("button-op-calculation-p1").innerHTML = optionsDropDownCalculo[i]
+        // console.log(optionsDropDownCalculo[i]);
     })
 }
