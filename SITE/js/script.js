e73373d84075b243357bd0de386b776a72577ea9
Paulo Sergio <pscosta2008@gmail.com>
2023-10-08 16:19:22 -0300

    Adicionando lógica do Sistema Price

--- SITE/js/script.js
+++ SITE/js/script.js
@@ -1,3 +1,7 @@
+let valorPresente = 30000;
+let juros = 2;
+let numMeses = 3;
+
 // HEADER
 
 function TiraDropDown(elemento){
@@ -66,14 +70,21 @@ const inputs = document.getElementsByClassName("inputs") // como tem vários el.
 
 for(let i = 0; i < inputs.length; i++){ // funcao para pegar valor de input e deixá-lo com 2 casas decimais
     inputs[i].addEventListener("change", function InputValues(){
+        
+
         let inputValue = Number(inputs[i].value)
-        if(i == 2){ // if para nao arredondar o input dos meses, visto que um mes nao pode ser 'quebrado'
-            inputValue = inputValue.toFixed(0); 
-            console.log(inputValue)
-            return;
+
+        if(i == 0){ 
+            valorPresente = inputValue.toFixed(2);
+            console.log("Valor Presente: " + valorPresente);
+        }if(i == 1){ 
+            juros = inputValue.toFixed(2);
+            console.log("Juros: " + juros);
+        }else if(i == 2){
+            numMeses = inputValue.toFixed(0); 
+            console.log("Mes: " + numMeses);
         }
-        inputValue = inputValue.toFixed(2) // funcao nativa para arredondar a 2 casas decimais (o valor precisa estar em numeros)
-        console.log(inputValue)
+
     })
 }
 
@@ -102,25 +113,59 @@ for(let i = 0; i < opButtonCalculo.length; i++){
 
 const optionsDropDownCalculo = ['PRICE', 'SAC', 'SACRE', 'MEJS'];
 const arrayElementLi = [document.getElementsByClassName("op-dropDown-calculo1"), document.getElementsByClassName("op-dropDown-calculo2")];
+const tbody = document.getElementsByClassName("tbody");
 
 for(let j = 0; j < arrayElementLi.length; j++){
     for(let i = 0; i < arrayElementLi[j].length; i++){
         arrayElementLi[j][i].addEventListener("click", () =>{
-
+            tbody[j].innerHTML = "";
             document.getElementsByClassName("button-op-calculation")[j].innerHTML = optionsDropDownCalculo[i]
             TiraDropDown(dropDownCalculo)
+
+            mudaTabela(i, valorPresente, numMeses, juros, tbody[j]);
+            
             abaButtonOpen = false;
-            if(i === 0){ // colocar formula para a tabela PRICE
-                console.log(optionsDropDownCalculo[i])
-            }else if(i === 1){ // SAC
-                console.log(optionsDropDownCalculo[i])
-            }else if(i === 2){ // SACRE
-                console.log(optionsDropDownCalculo[i])
-            }else if(i === 3){ // MEJS
-                console.log(optionsDropDownCalculo[i])
-            }
         })
 
     }
 }
 
+function linhaTabela(tabela, mes, valorParcela, amortizacao, juros, saldoDevedor){
+    let linhaTabela = document.createElement("tr");
+    linhaTabela.innerHTML = 
+    `
+        <td class="n-td">${mes}</td>
+        <td class="valorParcela-td">R$ ${valorParcela.toFixed(2)}</td>
+        <td class="amortizacao-td">R$ ${amortizacao.toFixed(2)}</td>
+        <td class="juros-td">R$ ${juros.toFixed(2)}</td>
+        <td class="saldoDevedor-td">R$ ${saldoDevedor.toFixed(2)}</td>
+    `
+    tabela.appendChild(linhaTabela);
+}
+
+function calculoPrice(emprestimo, numMeses, taxa, tabela){
+    let valorParcela = emprestimo * (taxa/100) / ( 1 - (1 + (taxa/100)) ** -numMeses)
+    let juros;
+    let amortizacao;
+    let saldoDevedor = emprestimo;
+
+    for(let i = 1; i <= numMeses; i++){
+        juros = saldoDevedor * (taxa/100);
+        amortizacao = valorParcela - juros;
+        saldoDevedor -= amortizacao;
+        
+        linhaTabela(tabela, i, valorParcela, amortizacao, juros, saldoDevedor)
+    }
+}
+
+function mudaTabela(i, valorPresente, numMeses, juros, tabela){
+    if(i === 0){ // colocar formula para a tabela PRICE
+        calculoPrice(valorPresente, numMeses, juros, tabela);
+    }else if(i === 1){ // SAC
+        console.log(optionsDropDownCalculo[i])
+    }else if(i === 2){ // SACRE
+        console.log(optionsDropDownCalculo[i])
+    }else if(i === 3){ // MEJS
+        console.log(optionsDropDownCalculo[i])
+    }
+}
